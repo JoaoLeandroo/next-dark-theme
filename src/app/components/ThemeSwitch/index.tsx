817bc3c497097a757d802be1d4cfa1273692d2a4
joaoleandro <new.joaoleandro@gmail.com>
2023-08-16 15:22:41 -0300

    dark theme

---
+++ src/app/components/ThemeSwitch/index.tsx
@@ -0,0 +1,44 @@
+"use client"
+import { useTheme } from 'next-themes'
+
+const IconMoon = () => {
+    return(
+    <svg 
+        className="bi bi-moon-stars-fill" 
+        fill="currentColor" 
+        height="25" 
+        viewBox="0 0 16 16" 
+        width="25" 
+        xmlns="http://www.w3.org/2000/svg">
+        <path d="M6 .278a.768.768 0 0 1 .08.858 7.208 7.208 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277.527 0 1.04-.055 1.533-.16a.787.787 0 0 1 .81.316.733.733 0 0 1-.031.893A8.349 8.349 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.752.752 0 0 1 6 .278z"/><path 
+        d="M10.794 3.148a.217.217 0 0 1 .412 0l.387 1.162c.173.518.579.924 1.097 1.097l1.162.387a.217.217 0 0 1 0 .412l-1.162.387a1.734 1.734 0 0 0-1.097 1.097l-.387 1.162a.217.217 0 0 1-.412 0l-.387-1.162A1.734 1.734 0 0 0 9.31 6.593l-1.162-.387a.217.217 0 0 1 0-.412l1.162-.387a1.734 1.734 0 0 0 1.097-1.097l.387-1.162zM13.863.099a.145.145 0 0 1 .274 0l.258.774c.115.346.386.617.732.732l.774.258a.145.145 0 0 1 0 .274l-.774.258a1.156 1.156 0 0 0-.732.732l-.258.774a.145.145 0 0 1-.274 0l-.258-.774a1.156 1.156 0 0 0-.732-.732l-.774-.258a.145.145 0 0 1 0-.274l.774-.258c.346-.115.617-.386.732-.732L13.863.1z"/>
+    </svg>
+    )
+}
+
+const IconSun = () => {
+    return(
+        <svg 
+            className="feather feather-sun" 
+            fill="none" 
+            height="30" 
+            stroke="currentColor" 
+            stroke-linecap="round" 
+            stroke-linejoin="round" 
+            stroke-width="2" 
+            viewBox="0 0 24 24" 
+            width="30" 
+            xmlns="http://www.w3.org/2000/svg">
+            <circle cx="12" cy="12" r="5"/><line x1="12" x2="12" y1="1" y2="3"/><line x1="12" x2="12" y1="21" y2="23"/><line x1="4.22" x2="5.64" y1="4.22" y2="5.64"/><line x1="18.36" x2="19.78" y1="18.36" y2="19.78"/><line x1="1" x2="3" y1="12" y2="12"/><line x1="21" x2="23" y1="12" y2="12"/><line x1="4.22" x2="5.64" y1="19.78" y2="18.36"/><line x1="18.36" x2="19.78" y1="5.64" y2="4.22"/>
+        </svg>
+    )
+}
+
+export default function ThemeSwitch() {
+    const { theme, setTheme } = useTheme()
+
+    return( 
+        <button onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
+            {theme === "dark" ? <IconSun/> : <IconMoon/>}
+        </button> )
+}
